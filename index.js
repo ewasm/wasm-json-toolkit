f89436db276533b655368005635984b47d52f064
wanderer <mjbecze@gmail.com>
2016-12-21 23:16:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 /**
- * converts a wasm binary into a json representation
+ * Converts a wasm binary into a json representation
  * @param {Buffer}
  * @return {Object}
  */
 exports.wasm2json = require('./wasm2json')
 
 /**
- * converts a json representation to a wasm binary
+ * Converts a json representation to a wasm binary
  * @param {Object}
  * @return {Buffer}
  */
 exports.json2wasm = require('./json2wasm')
 
 /**
- * converts text to json. The only text accepted is a simple list of opcode name and immediates
+ * Converts text to json. The only accepts text that is a simple list of opcode name and immediates
  * @param {String}
  * @return {Object}
  * @example
